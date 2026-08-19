80e9bea4be68c9f743e42ae7c2ee39f2e80f8164
Miguel Gutierrez <miguel0504@live.com>
2026-08-18 20:11:03 -0500

    Se crea proyecto, se agrega gitignore.

---
+++ Parking/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Parking
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
